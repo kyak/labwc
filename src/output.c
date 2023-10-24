dca0adaed171ff50b5b047d31d846ed68cb92103
John Lindgren <john@jlindgren.net>
2023-10-24 13:49:15 -0400

    xwayland: set _NET_WORKAREA property based on usable area
    XWayland clients use the _NET_WORKAREA root window property to determine
    how much of the screen is not covered by panels/docks. The property is
    used for example by Qt to determine areas of the screen that popup menus
    should not overlap (see QScreen::availableVirtualGeometry).
    
    Depends on wlroots MR:
    https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/4406
    
    v2: prevent calling wlr_xwayland_set_workareas() too early

--- src/output.c
+++ src/output.c
@@ -622,6 +622,9 @@ output_update_usable_area(struct output *output)
 {
 	if (update_usable_area(output)) {
 		regions_update_geometry(output);
+#if HAVE_XWAYLAND
+		xwayland_update_workarea(output->server);
+#endif
 		desktop_arrange_all_views(output->server);
 	}
 }
@@ -641,6 +644,9 @@ output_update_all_usable_areas(struct server *server, bool layout_changed)
 		}
 	}
 	if (usable_area_changed || layout_changed) {
+#if HAVE_XWAYLAND
+		xwayland_update_workarea(server);
+#endif
 		desktop_arrange_all_views(server);
 	}
 }
